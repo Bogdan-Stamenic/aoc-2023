8c0227b62d7351256ce30482e5687cd3f1d0b421
Bogdan Stamenic <bogdan.stamenic@hotmail.com>
2023-12-20 19:47:38 +0100

    day17: solved p2

--- src/day17.rs
+++ src/day17.rs
@@ -17,8 +17,7 @@ pub struct HeatlossNode {
     travelled: u32,
 }
 
-#[aoc_generator(day17)]
-pub fn input_generator(input: &str) -> (DiGraphMap<HeatlossNode,u32>, (usize, usize)) {
+pub fn input_generator_p1(input: &str) -> (DiGraphMap<HeatlossNode,u32>, (usize, usize)) {
     let grid = input.lines()
         .map(|line| {
             line.chars()
@@ -31,13 +30,13 @@ pub fn input_generator(input: &str) -> (DiGraphMap<HeatlossNode,u32>, (usize, us
     let mut out = DiGraphMap::<HeatlossNode,u32>::new();
     for i in 0..outer_max {
         for j in 0..inner_max {
-            connect_edges(&mut out, &grid, i, j);
+            connect_edges_p1(&mut out, &grid, i, j);
         }
     }
     (out, (outer_max - 1, inner_max - 1))
 }
 
-fn connect_edges(graph: &mut DiGraphMap<HeatlossNode,u32>, grid: &Vec<Vec<u32>>, i: usize, j: usize) {
+fn connect_edges_p1(graph: &mut DiGraphMap<HeatlossNode,u32>, grid: &Vec<Vec<u32>>, i: usize, j: usize) {
     if (i == 0) && (j == 0) {
         /* special start node */
         let strt = HeatlossNode {coords: (0,0), dir: Direction::Start, travelled: 0};
@@ -97,19 +96,101 @@ fn calc_dist_travelled(curr_direction: &Direction, next_direction: Direction, di
 }
 
 #[aoc(day17, part1)]
-pub fn solve_part1(input: &(DiGraphMap<HeatlossNode,u32>, (usize,usize))) -> u32 {
+pub fn solve_part1(input: &str) -> u32 {
+    let input = input_generator_p1(input);
     let (input, last_node) = input;
     let strt = HeatlossNode {coords: (0,0), dir: Direction::Start, travelled: 0};
     let ans = dijkstra(&input, strt, None, |edge_ref| *edge_ref.2);
     ans.into_iter()
-        .filter(|el| el.0.coords == *last_node)
+        .filter(|el| el.0.coords == last_node)
         .map(|el| el.1)
         .min().unwrap()
 }
 
-//#[aoc(day17, part2)]
-//pub fn solve_part2(input: &str) -> u64 {
-//}
+pub fn input_generator_p2(input: &str) -> (DiGraphMap<HeatlossNode,u32>, (usize, usize)) {
+    let grid = input.lines()
+        .map(|line| {
+            line.chars()
+                .map(|ch: char| ch.to_string().parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()
+        })
+    .collect::<Vec<Vec<u32>>>();
+    let outer_max = grid.len();
+    let inner_max = grid[0].len();
+    let mut out = DiGraphMap::<HeatlossNode,u32>::new();
+    for i in 0..outer_max {
+        for j in 0..inner_max {
+            connect_edges_p2(&mut out, &grid, i, j);
+        }
+    }
+    (out, (outer_max - 1, inner_max - 1))
+}
+
+fn connect_edges_p2(graph: &mut DiGraphMap<HeatlossNode,u32>, grid: &Vec<Vec<u32>>, i: usize, j: usize) {
+    if (i == 0) && (j == 0) {
+        /* special start node */
+        let strt = HeatlossNode {coords: (0,0), dir: Direction::Start, travelled: 0};
+        let below_strt = HeatlossNode {coords: (1,0), dir: Direction::Down, travelled:  1};
+        let right_of_strt = HeatlossNode {coords: (0,1), dir: Direction::Right, travelled:  1};
+        graph.add_edge(strt, below_strt, grid[1][0]);
+        graph.add_edge(strt, right_of_strt, grid[0][1]);
+    } else {
+        /* all other nodes */
+        for direction in vec![Direction::Left, Direction::Right, Direction::Up, Direction::Down].iter() {
+            for dist_travelled in 1..=10 {
+                /* curent node */
+                let foo = HeatlossNode {coords: (i,j), dir: *direction, travelled: dist_travelled};
+                /* Don't go out of bounds && No 180° turns && Don't travel more than 3 tiles in one
+                 * direction*/
+                if (i < grid.len() - 1) && (*direction != Direction::Up) && p2_dist_check(&foo, Direction::Down){
+                    let new_travelled = calc_dist_travelled(direction, Direction::Down, dist_travelled);
+                    let bar = HeatlossNode {coords: (i + 1,j), dir: Direction::Down, travelled: new_travelled};
+                    graph.add_edge(foo, bar, grid[i + 1][j]);
+                }
+                if (i > 0) && (*direction != Direction::Down) && p2_dist_check(&foo, Direction::Up) {
+                    let new_travelled = calc_dist_travelled(direction, Direction::Up, dist_travelled);
+                    let bar = HeatlossNode {coords: (i - 1,j), dir: Direction::Up, travelled: new_travelled};
+                    graph.add_edge(foo, bar, grid[i - 1][j]);
+                }
+                if (j < grid[0].len() - 1) && (*direction != Direction::Left) && p2_dist_check(&foo, Direction::Right){
+                    let new_travelled = calc_dist_travelled(direction, Direction::Right, dist_travelled);
+                    let bar = HeatlossNode {coords: (i,j + 1), dir: Direction::Right, travelled: new_travelled};
+                    graph.add_edge(foo, bar, grid[i][j + 1]);
+                }
+                if (j > 0) && (*direction != Direction::Right) && p2_dist_check(&foo, Direction::Left) {
+                    let new_travelled = calc_dist_travelled(direction, Direction::Left, dist_travelled);
+                    let bar = HeatlossNode {coords: (i,j - 1), dir: Direction::Left, travelled: new_travelled};
+                    graph.add_edge(foo, bar, grid[i][j - 1]);
+                }
+            }
+        }
+    }
+}
+
+/* Travel between 4 and 10 tiles */
+#[inline]
+fn p2_dist_check(node: &HeatlossNode, direction: Direction) -> bool {
+    if (node.dir != direction) && (node.travelled < 4) {
+        return false;
+    }
+    if (node.dir == direction) && (node.travelled == 10) {
+        return false;
+    }
+    true
+}
+
+#[aoc(day17, part2)]
+pub fn solve_part2(input: &str) -> u32 {
+    let input = input_generator_p2(input);
+    let (input, last_node) = input;
+    let strt = HeatlossNode {coords: (0,0), dir: Direction::Start, travelled: 0};
+    let ans = dijkstra(&input, strt, None, |edge_ref| *edge_ref.2);
+    println!("{}", ans.is_empty());
+    ans.into_iter()
+        .filter(|el| el.0.coords == last_node)
+        .map(|el| el.1)
+        .min().expect("End is unreachable")
+}
 
 #[cfg(test)]
 mod tests {
@@ -131,8 +212,7 @@ mod tests {
 
     #[test]
     fn day17_p1_1() {
-        let input = input_generator(TEST_INPUT);
-        let ans = solve_part1(&input);
+        let ans = solve_part1(TEST_INPUT);
         assert_eq!(ans, 102);
     }
 
@@ -142,8 +222,7 @@ mod tests {
 91199
 99119
 99911";
-        let input = input_generator(INPUT);
-        let ans = solve_part1(&input);
+        let ans = solve_part1(INPUT);
         assert_eq!(ans, 7);
     }
 
@@ -160,8 +239,7 @@ mod tests {
 //9911.
 //99.1.
 //9999.
-        let input = input_generator(INPUT);
-        let ans = solve_part1(&input);
+        let ans = solve_part1(INPUT);
         assert_eq!(ans, 1);
     }
 
@@ -175,15 +253,13 @@ mod tests {
 019999
 099009
 000000";
-        let input = input_generator(INPUT);
-        let ans = solve_part1(&input);
+        let ans = solve_part1(INPUT);
         assert_eq!(ans, 1);
     }
 
-
-
-
-    //#[test]
-    //fn day17_p2() {
-    //}
+    #[test]
+    fn day17_p2_1() {
+        let ans = solve_part2(TEST_INPUT);
+        assert_eq!(ans, 94);
+    }
 }
